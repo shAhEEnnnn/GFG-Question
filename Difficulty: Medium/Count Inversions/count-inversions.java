f3c1f5527d22bb31d29d510b62286ccf250df53f
Shaheen Parween <shaheenamanat810@gmail.com>
2026-02-17 15:59:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Inversions/count-inversions.java
@@ -0,0 +1,42 @@
+class Solution {
+    static int count;
+    static int inversionCount(int arr[]) {
+        count = 0;
+        mergeSort(arr);
+        return count;
+    }
+        public static void mergeSort(int[] arr){
+        int n= arr.length;
+        if(n==1) return; 
+        int[] a = new int[n/2]; 
+        int[] b = new int[n-n/2];
+        
+        int idx = 0; 
+        for(int i=0;i<a.length;i++) a[i] = arr[idx++];
+        for(int i=0;i<b.length;i++) b[i] = arr[idx++];
+      
+        mergeSort(a); 
+        mergeSort(b);
+        
+        merge(a, b, arr);
+
+    }
+    public static void merge(int[] a, int[] b, int[] c){
+        int i=0, j=0, k=0;
+        while(i<a.length && j<b.length){
+            if(a[i]<=b[j]) {
+                c[k++] = a[i++];
+            }
+            else {
+                count += (a.length-i);
+                c[k++] = b[j++];
+        }
+    }
+        while(i<a.length) c[k++] = a[i++];
+        while(j<b.length) c[k++] = b[j++];
+    }
+    
+}
+
+       
+   
